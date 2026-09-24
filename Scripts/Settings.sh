d11e09b5e6c9cddf684619dfce9388159b963db1
sy3tem <sy3tem@users.noreply.github.com>
2026-09-24 19:48:56 +0800

    Revert "feat: 删除系统-插件菜单, 去掉状态页编译者+日期后缀"
    This reverts commit 4f96e441dc5ab4d5d962d4b7d529bf809ad05a84.

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -8,11 +8,8 @@ sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "M
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改immortalwrt.lan关联IP
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-#状态页不再追加编译信息(编译者+日期)后缀
-#删除系统-插件菜单(ImmortalWrt 自带 plugins 页面, 本固件用不到)
-find ./feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/ -type f -name "*.json" -exec sed -i '/admin\/system\/plugins/d' {} +
-find ./feeds/luci/modules/luci-mod-system/ -type f -name "plugins.js" -delete
-echo "system plugins page removed!"
+#添加编译日期标识
+sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 
 WIFI_SH=$(find ./target/linux/{mediatek/filogic,qualcommax}/base-files/etc/uci-defaults/ -type f -name "*set-wireless.sh" 2>/dev/null)
 WIFI_UC="./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
