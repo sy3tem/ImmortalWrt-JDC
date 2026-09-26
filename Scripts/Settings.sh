63633b598dbb65c296290aa61ac855d012312b2c
sy3tem <sy3tem@users.noreply.github.com>
2026-09-26 18:00:00 +0800

    fix(apk源): 依赖链是IMAGEOPT->VERSIONOPT->VERSION_REPO三级,补上CONFIG_IMAGEOPT=y,否则VERSIONOPT/REPO都被kconfig丢弃

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -45,8 +45,10 @@ echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
 #apk软件源改国内镜像(SJTU上海交大, snapshots滚动版; 刷机后apk install走国内不卡)
 #VERSION_REPO 是编译变量, 写进 /etc/apk/repositories.d/distfeeds.list
-#坑: CONFIG_VERSION_REPO 在 image-config.in 里被 "if VERSIONOPT ... endif" 包裹,
-#    必须先开 CONFIG_VERSIONOPT=y 否则 kconfig(olddefconfig) 会把 VERSION_REPO 丢弃, 源不生效(之前刷的旧版就是这样白改了)
+#坑: 依赖链 CONFIG_IMAGEOPT -> CONFIG_VERSIONOPT -> CONFIG_VERSION_REPO, 三级都要=y
+#    VERSION_REPO 在 image-config.in 里被 "if VERSIONOPT" 包裹, 而 VERSIONOPT 又是 "if IMAGEOPT" 的 menuconfig(default n)
+#    少开任何一个, kconfig(olddefconfig) 都会把下级的 REPO 丢弃, 源静默不生效(25ebdcf/d1ea589/2f8abc2 三批都因此白改)
+echo 'CONFIG_IMAGEOPT=y' >> ./.config
 echo 'CONFIG_VERSIONOPT=y' >> ./.config
 echo 'CONFIG_VERSION_REPO="https://mirrors.sjtug.sjtu.edu.cn/immortalwrt/snapshots"' >> ./.config
 
